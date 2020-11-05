5c1721f94a28269cac40a2d66404e7dc432b7ec2
Fog <lin.emay@gmail.com>
2020-11-05 01:57:00 -0800

    fix apiRoute error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,19 +14,19 @@ module.exports = (app) => {
   });
 
   //Create workout
-  app.post("/api/workouts", (req, res) => {
-    workouts.create({})
-      .then(workout => {
-        res.json(workout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+  app.post("/api/workouts", async (req, res)=> {
+    try{
+        const response = await db.Workout.create({type: "workout"})
+        res.json(response);
+    }
+    catch(err){
+        console.log("error occurred creating a workout: ", err)
+    }
+})
 
 //Update workout
   app.put("/api/workouts/:id", (req, res) => {
-    workouts.findByIdAndUpdate(
+    db.Workout.findByIdAndUpdate(
       req.params.id,
       { $push: { exercises: req.body } },
 
